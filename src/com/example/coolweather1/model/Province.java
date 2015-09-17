6b0e373e01d2b5ff7056c5f19ec8b03767c83d7d
LiangFeng <742939192@qq.com>
2015-09-17 19:49:43 +0800

    新增数据库帮助ç”±»，以及各表对应的史æå®ä的实体类。“

---
+++ src/com/example/coolweather1/model/Province.java
@@ -0,0 +1,43 @@
+package com.example.coolweather1.model;
+
+public class Province {
+	
+	private int id;
+	
+	private String provinceName;
+	
+	private String provinceCode;
+	
+	
+	public int getId()
+	{
+		return id;
+	}
+	
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+	
+	public String getProvinceName()
+	{
+		return provinceName;
+	}
+	
+	public void setProvinceName(String name)
+	{
+	    this.provinceName = name;	
+	}
+	
+	public String getProvinceCode()
+	{
+		return provinceCode;
+	}
+	
+	
+	public void setProvinceCode(String provinceCode)
+	{
+		this.provinceCode = provinceCode;
+	}
+
+}
